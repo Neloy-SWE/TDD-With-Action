580ab6d2b6f6a92ed421d8a4f36842093ae329de
Neloy-SWE <taufiqneloy.swe@gmail.com>
2025-06-02 22:24:30 +0600

    group test add

--- test/counter_test.dart
+++ test/counter_test.dart
@@ -11,4 +11,28 @@ void main() {
     Counter counter = Counter();
     expect(counter.value, 0);
   });
+  group("check method: increment, decrement", (){
+    test("check method: increment", (){
+      Counter counter = Counter();
+      expect(counter.value, 0);
+
+      counter.increment();
+      expect(counter.value, 1);
+
+      counter.increment();
+      expect(counter.value, 2);
+    });
+
+    test("check method: decrement", (){
+      Counter counter = Counter();
+      expect(counter.value, 0);
+
+      counter.decrement();
+      expect(counter.value, -1);
+
+      counter.decrement();
+      expect(counter.value, -2);
+    });
+  });
+
 }
