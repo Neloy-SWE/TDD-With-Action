087976a3a5fac55568facccf528e990529093526
Neloy-SWE <taufiqneloy.swe@gmail.com>
2025-06-14 00:07:23 +0600

    album model class added

---
+++ lib/network/model/model_album.dart
@@ -0,0 +1,23 @@
+/*
+* Created by Neloy on 13 June, 2025.
+* Email: taufiqneloy.swe@gmail.com
+*/
+
+class Album {
+  final int userId;
+  final int id;
+  final String title;
+
+  const Album({required this.userId, required this.id, required this.title});
+
+  factory Album.fromJson(Map<String, dynamic> json) {
+    return switch (json) {
+      {'userId': int userId, 'id': int id, 'title': String title} => Album(
+        userId: userId,
+        id: id,
+        title: title,
+      ),
+      _ => throw const FormatException('Failed to load album.'),
+    };
+  }
+}
