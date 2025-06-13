cde5468a894e6ba0505b589d669e9c4085e5fae0
Neloy-SWE <taufiqneloy.swe@gmail.com>
2025-06-14 00:09:32 +0600

    test cases added

---
+++ test/fetch_album_test.dart
@@ -0,0 +1,43 @@
+/*
+* Created by Neloy on 13 June, 2025.
+* Email: taufiqneloy.swe@gmail.com
+*/
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart' as http;
+// import 'package:mocking/main.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:tdd_git_action/network/api/api_album.dart';
+import 'package:tdd_git_action/network/model/model_album.dart';
+
+import 'fetch_album_test.mocks.dart';
+
+
+@GenerateMocks([http.Client])
+void main() {
+  group("fetch album", (){
+    test("album fetch successful", () async {
+      final client = MockClient();
+
+      when(
+        client.get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1')),
+      ).thenAnswer(
+            (_) async =>
+            http.Response('{"userId": 1, "id": 2, "title": "mock"}', 200),
+      );
+
+      expect(await fetchAlbum(client), isA<Album>());
+    });
+
+    test("album fetch fail", () async{
+      final client = MockClient();
+
+      when(
+        client.get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1')),
+      ).thenAnswer((_) async => http.Response('Not Found', 404));
+
+      expect(fetchAlbum(client), throwsException);
+    });
+  });
+}
